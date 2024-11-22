21d6a7fa522122f5282b2eab7d49390a13ba5d39
hola-marcus <hola_marcus@icloud.com>
2024-11-21 19:38:59 -0600

    Update news_stock.py

--- akshare/news/news_stock.py
+++ akshare/news/news_stock.py
@@ -13,14 +13,6 @@ import requests
 
 
 def stock_news_em(symbol: str = "300059") -> pd.DataFrame:
-    """
-    东方财富-个股新闻-最近 100 条新闻
-    https://so.eastmoney.com/news/s?keyword=%E4%B8%AD%E5%9B%BD%E4%BA%BA%E5%AF%BF&pageindex=1&searchrange=8192&sortfiled=4
-    :param symbol: 股票代码
-    :type symbol: str
-    :return: 个股新闻
-    :rtype: pandas.DataFrame
-    """
     url = "http://search-api-web.eastmoney.com/search/jsonp"
     params = {
         "cb": "jQuery3510875346244069884_1668256937995",
